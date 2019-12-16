048a92ab385a796ed9599833ff075372a9ce9e64
ateliee <ateliee@gmail.com>
2019-12-16 09:15:27 +0900

    minify support

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
+
+//js minify
+gulp.task('js-minify', function() {
+    return gulp.src(['./js/*.js', '!./js/*.min.js'])
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest('./dist/js/'))
+    ;
+});
